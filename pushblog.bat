5815c0d554f921095b69105d762e067bfb3aa6af
dreamerhe114514 <dreamerhe7747@hotmail.com>
2024-05-04 20:40:50 +0800

    涓婁紶鍗氬

--- pushblog.bat
+++ pushblog.bat
@@ -2,4 +2,4 @@ git config --global user.name "dreamerhe114514"
 git config --global user.email "dreamerhe7747@hotmail.com"
 git add .
 git commit -m "上传博客"
-git push origin master
+git push origin main
